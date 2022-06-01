f2f9ae957f33de72e1ccda2d257f92677bda93eb
Bogdan <42344818+winfar3@users.noreply.github.com>
2022-06-01 13:24:39 +0300

    fixed bug when const elementMain created before render whith nill

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,74 +1,77 @@
 import "./Main.scss";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { PostCard } from "../../components/PostCard/PostCard";
 import { Pagination } from "../../components/Pagination/Pagination";
-import { data } from '../../database/database.js';
+import { data } from "../../database/database.js";
 
 export function Main() {
-    const [postCardData] = useState(data);
+  const [postCardData] = useState(data);
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(7);
-    const maxPageNumber = Math.ceil(postCardData.length / postsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(7);
+  const maxPageNumber = Math.ceil(postCardData.length / postsPerPage);
 
-    const lastPostIndex = currentPage * postsPerPage;
-    const firstPostIndex = lastPostIndex - postsPerPage;
-    const currentPosts = postCardData.slice(firstPostIndex, lastPostIndex);
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = postCardData.slice(firstPostIndex, lastPostIndex);
 
-    const elementMain = document.querySelector('.main');
-    const scrollToTop = () => {
-        elementMain.scrollIntoView({
-            behavior: 'smooth'
-        });
+  const [elementMain, setElementMain] = useState();
+  useEffect(() => {
+    setElementMain(document.querySelector(".main"));
+  }, [elementMain]);
+  
+  const scrollToTop = () => {
+    elementMain.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+	/** COMMENT
+   * Пытался сделать прокрутку к началу списка постов при пагинации.
+   * Но через useEffect прокрутка происходила так же при загрузке страницы.
+   * Остановился на варианте сделать просто функцию.
+   * Вопрос: нужно ли здесь использовать useEffect вообще?
+   * Если да, то как убрать прокрутку при загрузке страницы и оставить только на переход между страницами?
+   */
+  // useEffect(() => {
+  //     const elementMain = document.querySelector('.main');
+
+  //     elementMain.scrollIntoView({
+  //         behavior: 'smooth'
+  //     });
+  // }, [currentPage]);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    scrollToTop();
+  };
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+      scrollToTop();
     }
+  };
+  const nextPage = () => {
+    if (currentPage < maxPageNumber) {
+      setCurrentPage((prev) => prev + 1);
+      scrollToTop();
+    }
+  };
 
-    const paginate = pageNumber => {
-        setCurrentPage(pageNumber);
-        scrollToTop();
-    };
-    const prevPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(prev => prev - 1);
-            scrollToTop();
-        }
-    };
-    const nextPage = () => {
-        if (currentPage < maxPageNumber) {
-            setCurrentPage(prev => prev + 1);
-            scrollToTop();
-        }
-    };
-
-    /** COMMENT
-     * Пытался сделать прокрутку к началу списка постов при пагинации. 
-     * Но через useEffect прокрутка происходила так же при загрузке страницы.
-     * Остановился на варианте сделать просто функцию.
-     * Вопрос: нужно ли здесь использовать useEffect вообще? 
-     * Если да, то как убрать прокрутку при загрузке страницы и оставить только на переход между страницами?
-     */
-    // TODO: fix scroll on click to 'home'
-    // useEffect(() => {
-    //     const elementMain = document.querySelector('.main');
-
-    //     elementMain.scrollIntoView({
-    //         behavior: 'smooth'
-    //     });
-    // }, [currentPage]);
-
-    return (
-        <main className="main">
-            {currentPosts.map(postCardItem =>
-                <PostCard post={postCardItem} key={postCardItem.id} />
-            )}
-            <Pagination 
-                maxPageNumber={maxPageNumber} 
-                postsPerPage={postsPerPage} 
-                paginate={paginate}
-                prevPage={prevPage}
-                nextPage={nextPage}
-            />
-        </main>
-    );
+  return (
+    <main className="main">
+      {currentPosts.map((postCardItem) => (
+        <PostCard post={postCardItem} key={postCardItem.id} />
+      ))}
+      <Pagination
+        maxPageNumber={maxPageNumber}
+        postsPerPage={postsPerPage}
+        paginate={paginate}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
+    </main>
+  );
 }
