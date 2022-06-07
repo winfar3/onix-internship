34a2b79a3ac539dc114ff2ca80808b13f9abf309
Bogdan <42344818+winfar3@users.noreply.github.com>
2022-06-07 20:12:49 +0300

    changed FeaturedPosts to show last 3 from api

--- src/components/FeaturedPosts/FeaturedPosts.js
+++ src/components/FeaturedPosts/FeaturedPosts.js
@@ -1,55 +1,20 @@
 import "./FeaturedPosts.scss";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import image12 from "../../assets/images/postcard/12.png"
-import image13 from "../../assets/images/postcard/13.png"
-import image14 from "../../assets/images/postcard/14.png"
 import FeaturedPostsView from "./FeaturedPostsView";
+import SendAxiosRequest from "../../database/SendAxiosRequest";
 
 export function FeaturedPosts() {
-    const [postCardData] = useState([{
-        id: 12,
-        cardSize: "small",
-        imageUrl: image12,
-        isPositionTop: false,
-        category: "jeans",
-        title: "One of Saturn’s largest rings may be newer than anyone",
-        date: "2019-05-14",
-        author: {
-            firstName: "Rickie",
-            lastName: "Baroch"
-        },
-        description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem"
-    }, {
-        id: 13,
-        cardSize: "small",
-        imageUrl: image13,
-        isPositionTop: false,
-        category: "City",
-        title: "One of Saturn’s largest rings may be newer than anyone",
-        date: "2019-05-13",
-        author: {
-            firstName: "Rickie",
-            lastName: "Baroch"
-        },
-        description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem"
-    }, {
-        id: 14,
-        cardSize: "small",
-        imageUrl: image14,
-        isPositionTop: false,
-        category: "Photography",
-        title: "One of Saturn’s largest rings may be newer than anyone",
-        date: "2019-05-11",
-        author: {
-            firstName: "Rickie",
-            lastName: "Baroch"
-        },
-        description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem"
-    }]);
+  const [featuredPostsArray, setFeaturedPostsArray] = useState([]);
 
-    return(
-        <FeaturedPostsView postCardData={postCardData}/>
-    );
+  useEffect(() => {
+    SendAxiosRequest()
+			.then((data) => {
+      	setFeaturedPostsArray(data.slice(-3));
+    	})
+			.catch((err) => console.log(err))
+  }, []);
+
+  return <FeaturedPostsView postCardData={featuredPostsArray} />;
 }
