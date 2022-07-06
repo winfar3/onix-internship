b88bf11d311636a6da66c07591c39fd6bc70a239
Bogdan <42344818+winfar3@users.noreply.github.com>
2022-07-06 16:13:29 +0300

    feat: added i18next-browser-languagedetector which detect the language of the site by the language of the browser and save/use localstorage

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,19 +1,26 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import languageDetector from 'i18next-browser-languagedetector';
 
 import translationsEn from './en';
 import translationsUa from './ua';
 import translationsRu from './ru';
 
+const options = {
+  order: ['localStorage', 'navigator'],
+};
+
 i18next
+  .use(languageDetector)
   .use(initReactI18next)
   .init({
+    detection: options,
     resources: {
       en: { translation: translationsEn },
       ua: { translation: translationsUa },
       ru: { translation: translationsRu },
     },
-    lng: 'en',
     fallbackLng: 'en',
+    supportedLngs: ['en', 'ua', 'ru'],
     interpolation: { escapeValue: false },
   });
