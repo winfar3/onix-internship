9e5ba11c7992960c66b3d234bec8d7cfe349baa3
Bogdan <42344818+winfar3@users.noreply.github.com>
2022-07-07 15:41:54 +0300

    feat: create useRequest to replace withRequest

---
+++ src/hooks/useRequest.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import SendAxiosRequest from '../helpers/SendAxiosRequest';
+
+const useRequest = (apiUrl) => {
+  const [requstedData, setRequstedData] = useState([]);
+  const [isPending, setIsPending] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+  
+    SendAxiosRequest(apiUrl)
+      .then((data) => {
+        setRequstedData(data);
+        setIsPending(false);
+      })
+      .catch(() => {
+        setRequstedData([]);
+        setIsPending(false);
+      });
+  }, []);
+
+  return [requstedData, isPending];
+};
+
+export default useRequest;
