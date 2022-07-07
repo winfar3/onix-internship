458c3b15104de84a19706298f39fa8443300ae41
Bogdan <42344818+winfar3@users.noreply.github.com>
2022-07-07 15:59:44 +0300

    fix: include useRequest to Hero instead of withRequest

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,35 +1,38 @@
 import './Hero.scss';
 
-import PropTypes from 'prop-types';
-
 import { lastRequestUrl } from '../../constants/requestUrls';
-import withRequest from '../../hocs/withRequest';
 import months from '../../constants/months';
 import useLocalization from '../../hooks/useLocalization';
 import HeroView from './HeroView';
+import useRequest from '../../hooks/useRequest';
 
 //  TODO hide loader
-function Hero({ dataFromServer }) {
+function Hero() {
   const [t] = useLocalization();
-  const [post] = dataFromServer;
-  const dateObject = new Date(post.date);
+  const [postData] = useRequest(lastRequestUrl);
+  let dateObject;
+  if (postData.length > 0) {
+    dateObject = new Date(postData[0].date);
+  }
+  
   return (
-    <HeroView 
-      id={post.id}
-      title={post.title}
-      dateMonth={t(months[dateObject.getMonth()])}
-      dateDay={dateObject.getDate()}
-      dateYear={dateObject.getFullYear()}
-      author={post.author}
-      imageUrl={post.imageUrl}
-      by={t('by')}
-      comments={t('comments')}
-    />
+    <>
+      {postData.map((post) => (
+        <HeroView 
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          dateMonth={t(months[dateObject.getMonth()])}
+          dateDay={dateObject.getDate()}
+          dateYear={dateObject.getFullYear()}
+          author={post.author}
+          imageUrl={post.imageUrl}
+          by={t('by')}
+          comments={t('comments')}
+        />
+      ))}
+    </>
   );
 }
 
-Hero.propTypes = {
-  dataFromServer: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-};
-
-export default withRequest(Hero, lastRequestUrl);
+export default Hero;
