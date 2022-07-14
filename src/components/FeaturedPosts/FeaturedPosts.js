db6c299788764b7ea76cf95b4a4c43c327d4a26a
Bogdan <42344818+winfar3@users.noreply.github.com>
2022-07-14 19:36:42 +0300

    refactor: use constants for requstUrl

--- src/components/FeaturedPosts/FeaturedPosts.js
+++ src/components/FeaturedPosts/FeaturedPosts.js
@@ -4,9 +4,11 @@ import { useEffect, useState } from 'react';
 
 import FeaturedPostsView from './FeaturedPostsView';
 import SendAxiosRequest from '../../helpers/SendAxiosRequest';
+import { baseUrl, postsRequestUrl } from '../../constants/requestUrls';
 
+// TODO: fix 'Can't perform a React state update on an unmounted component'
 function FeaturedPosts() {
-  const requstUrl = 'https://61fc04453f1e34001792c787.mockapi.io/posts';
+  const requstUrl = `${baseUrl}${postsRequestUrl}`;
   const [featuredPostsArray, setFeaturedPostsArray] = useState([]);
 
   useEffect(() => {
