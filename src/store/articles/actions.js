8cfd5ddc5f65e2e6faa6bc650257a65ef668eb98
Bogdan <42344818+winfar3@users.noreply.github.com>
2022-07-21 12:24:57 +0300

    fix: added async and await

--- src/store/articles/actions.js
+++ src/store/articles/actions.js
@@ -2,8 +2,8 @@ import SendAxiosRequest from '../../helpers/SendAxiosRequest';
 import FILLING_STORAGE from './types';
 
 const fillingStorageAction = (url, setIsPending) => {
-  return (dispatch) => {
-    SendAxiosRequest(url)
+  return async (dispatch) => {
+    await SendAxiosRequest(url)
       .then((data) => {
         dispatch({ type: FILLING_STORAGE, storeData: data });
         setIsPending(false);
