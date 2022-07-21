bcd9128c880174ad8f8b9fcd0339a542e4c8bbcb
Bogdan <42344818+winfar3@users.noreply.github.com>
2022-07-21 12:37:50 +0300

    fix: removed async/await

--- src/store/articles/actions.js
+++ src/store/articles/actions.js
@@ -2,8 +2,8 @@ import SendAxiosRequest from '../../helpers/SendAxiosRequest';
 import FILLING_STORAGE from './types';
 
 const fillingStorageAction = (url, setIsPending) => {
-  return async (dispatch) => {
-    await SendAxiosRequest(url)
+  return (dispatch) => {
+    SendAxiosRequest(url)
       .then((data) => {
         dispatch({ type: FILLING_STORAGE, storeData: data });
         setIsPending(false);
--- src/store/users/actions.js
+++ src/store/users/actions.js
@@ -2,8 +2,8 @@ import SendAxiosRequest from '../../helpers/SendAxiosRequest';
 import USERS_HANDLER from './types';
 
 const usersHandler = (url, setRefreshing, setIsPending) => {
-  return async (dispatch) => {
-    await SendAxiosRequest(url)
+  return (dispatch) => {
+    SendAxiosRequest(url)
       .then((data) => {
         dispatch({ type: USERS_HANDLER, storeData: data });
         setRefreshing(false);
